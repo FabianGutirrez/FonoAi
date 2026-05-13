95125d11fa1915d96b49f211c989ba327cdd0edb
FabianGutirrez <151792223+FabianGutirrez@users.noreply.github.com>
2026-05-13 21:23:34 +0000

    prueba a error

--- api/index.ts
+++ api/index.ts
@@ -40,7 +40,7 @@ app.post("/api/transcribe", (req, res, next) => {
     console.log("Servidor: Iniciando proceso de transcripción");
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({ 
-      model: "gemini-1.5-flash",
+      model: "gemini-1.5-flash-latest",
     });
 
     const transcriptionPrompt = req.body.prompt || "Transcribe este video exactamente.";
